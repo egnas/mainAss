f95e203e4ee43d120fd8a334be579e275cad4247
egnas <m019986e@student.staffs.ac.uk>
2017-05-07 00:37:49 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -20,3 +20,34 @@ button.addEventListener("click", function()
     document.getElementById("mobile").style.display = "none";
   }
 });
+
+
+const myForm = document.getElementById("myForm");
+if(myForm){
+    const myFormSubmit = e => {
+        e.preventDefault();
+        const contact = {
+            "name": e.target.customer_name.value,
+            "email": e.target.email_address.value,
+            "number": e.target.customer_number.value,
+            "choice": e.target.radioChoice.value,
+        };
+
+        fetch("http://cteth.com", {mode: "no-cors", method: "POST", body: contact})
+            .then(response => {
+                console.log('hello response!', response);
+            }).catch(() => {
+                let allContacts = [];
+
+                const existingContacts = localStorage.getItem('contact');
+                if(existingContacts){
+                    allContacts = JSON.parse(existingContacts);
+                }
+
+                allContacts.push(contact);
+                localStorage.setItem('contact', JSON.stringify(allContacts));
+                console.log(allContacts);
+            });
+    };
+    myForm.addEventListener('submit', myFormSubmit, false);
+}
